4041415442ca865b8bb7c35b916d938e5527f0f7
Johann <154441181+johann-l@users.noreply.github.com>
2025-05-01 17:19:23 +0530

    first

---
+++ main.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { app, BrowserWindow} = require('electron');
+const isWin = process.platform === 'win32';
+const isDev = process.env.NODE_ENV === 'development';
+
+function createMainWindow(){
+ const win = new BrowserWindow({
+    title: "FleetNow",
+    width: 1920,
+    height: 1080,
+    fullscreen: true
+ });
+ if (isDev) { 
+    win.webContents.openDevTools();
+ }
+ win.loadFile(path.join(__dirname, './renderer/pages/reports.html'));
+ win.maximize();
+}
+
+app.on('ready', () => {
+    if(!isWin){
+        app.quit();
+    }
+    createMainWindow();
+})
+
+const { autoUpdater } = require("electron-updater");
+
+app.on('ready', () => {
+  autoUpdater.checkForUpdatesAndNotify();
+});

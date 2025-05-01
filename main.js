aa0b753666f328e64c9d76156227cb2bce6bedb6
Johann <154441181+johann-l@users.noreply.github.com>
2025-05-01 18:42:25 +0530

    testing update v2

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
 const path = require('path');
-const { app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
 const isWin = process.platform === 'win32';
 const isDev = process.env.NODE_ENV === 'development';
 
-function createMainWindow(){
- const win = new BrowserWindow({
+function createMainWindow() {
+  const win = new BrowserWindow({
     title: "FleetNow",
     width: 1920,
     height: 1080,
     fullscreen: true
- });
- if (isDev) { 
+  });
+
+  if (isDev) {
     win.webContents.openDevTools();
- }
- win.loadFile(path.join(__dirname, './renderer/pages/index.html'));
- win.maximize();
+  }
+
+  win.loadFile(path.join(__dirname, './renderer/pages/index.html'));
+  win.maximize();
 }
 
 app.on('ready', () => {
-    if(!isWin){
-        app.quit();
-    }
-    createMainWindow();
-})
+  if (!isWin) {
+    app.quit(); // Quit early on unsupported OS
+    return;
+  }
 
-const { autoUpdater } = require("electron-updater");
-
-app.on('ready', () => {
+  createMainWindow();
   autoUpdater.checkForUpdatesAndNotify();
 });
